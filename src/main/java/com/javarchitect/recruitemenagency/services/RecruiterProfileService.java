283ff3fc082cf9fd384b249c432595692d7beaf5
rtaylor02 <97712087+rtaylor02@users.noreply.github.com>
2025-08-20 08:09:24 +0100

    Modify recruiter profile for more functionalities

---
+++ src/main/java/com/javarchitect/recruitemenagency/services/RecruiterProfileService.java
@@ -0,0 +1,27 @@
+package com.javarchitect.recruitemenagency.services;
+
+import com.javarchitect.recruitemenagency.entity.RecruiterProfile;
+import com.javarchitect.recruitemenagency.repository.RecruiterProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+
+    private final RecruiterProfileRepository recruiterRepository;
+
+    @Autowired
+    public RecruiterProfileService(RecruiterProfileRepository recruiterRepository) {
+        this.recruiterRepository = recruiterRepository;
+    }
+
+    public Optional<RecruiterProfile> getOne(Integer id) {
+        return recruiterRepository.findById(id);
+    }
+
+    public RecruiterProfile addNew(RecruiterProfile recruiterProfile) {
+        return recruiterRepository.save(recruiterProfile);
+    }
+}
